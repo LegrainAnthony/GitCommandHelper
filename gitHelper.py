9a7cfbc7599e43bc439367d295970ca7840e2706
Anthony Legrain <legrain.anthony51@gmail.com>
2023-02-12 23:14:44 +0100

    Git merge option

--- gitHelper.py
+++ gitHelper.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-choice = input("Que voulez-vous faire ?\n 1 - commit et push sur la branche actuelle \n 2 - git  \n 4 - test3 \n 5 - Créer une nouvelle branche \n 6 - test5 \n 7 - test6 \n 8 - test7 \n 9 - test8 \n 0 - pull depuis master \n q - Quitter \n Votre choix :")
+choice = input("Que voulez-vous faire ?\n 1 - commit et push sur la branche actuelle \n 2 - git  \n 4 - test3 \n 5 - Créer une nouvelle branche \n 6 - git push setupstream \n 7 - choix de branch + git merge \n 8 - test7 \n 9 - test8 \n 0 - pull depuis master \n q - Quitter \n Votre choix :")
 
 if choice == "1":
     message = input("Entrez le message de commit : ")
@@ -25,9 +25,32 @@ elif choice == "5":
     subprocess.run(["git", "checkout", "-b", branchName ])
 elif choice == "6":
     branchName = str(input("Entrez le nom de la branche : "))
-    subprocess.run(["git", "push", "--set-upstream", branchName ])
+    subprocess.run(["git", "push", "--set-upstream", "origin", branchName ])
 elif choice == "7":
-    print("test7")
+    choiceForPull = input("Voulez vous pull avant de merge ? \n y/n : ")
+    while choiceForPull != "y" and choiceForPull != "n":
+        choiceForPull = input("y/n : ")
+    if choiceForPull == "y":
+        subprocess.run(["git", "pull"])
+        choiceBranch = input("Voulez vous merge sur la branche actuelle ? \n y/n : ")
+        while choiceBranch != "y" and choiceBranch != "n":
+            choiceBranch = input("y/n : ")
+        if choiceBranch == "y":
+            subprocess.run(["git", "merge"])
+        elif choiceBranch == "n":
+            branchName = str(input("Entrez le nom de la branche : "))
+            subprocess.run(["git", "checkout", branchName ])
+            subprocess.run(["git", "merge"])
+    elif choiceForPull == "n":
+        choiceBranch = input("Voulez vous merge sur la branche actuelle ? \n y/n : ")
+        while choiceBranch != "y" and choiceBranch != "n":
+            choiceBranch = input("y/n : ")
+        if choiceBranch == "y":
+            subprocess.run(["git", "merge"])
+        elif choiceBranch == "n":
+            branchName = str(input("Entrez le nom de la branche : "))
+            subprocess.run(["git", "checkout", branchName ])
+            subprocess.run(["git", "merge"])
 elif choice == "8":
     print("test8")
 elif choice == "9":
